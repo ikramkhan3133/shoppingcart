811e1f0e9722e7c9598b4741dc0260b9272e624a
ikramkhan <khanikram3133@yahoo.com>
2017-09-14 15:47:09 -0700

    Added First Project

---
+++ ShoppingCartSPA1/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace ShoppingCartSPA1
+{
+    public static class WebApiConfig
+    {
+        public static string UrlPrefix { get { return "api"; } }
+        public static string UrlPrefixRelative { get { return "~/api"; } }
+
+        public static void Register(HttpConfiguration config)
+        {  
+            
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ShoppingCartSPA1/Controllers/ShoppingCartController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ShoppingCartSPA1.Controllers
+{
+    public class Item
+    {
+        public int Count { get; set; }
+    }
+    public class ShoppingCart
+    {
+        public List<Item> Items { get; set; }
+
+    }
+    public class ShoppingCartController : ApiController
+    {
+
+        [Route("api/GetProducts")]
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            var db = new ShoppingCartEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Products.ToList();
+        }
+
+        public void AddToCart(Item item)
+        {
+            ShoppingCart sc = null;
+
+            if (HttpContext.Current.Session["sc"] == null)
+                sc = new ShoppingCart();
+            else
+                sc = HttpContext.Current.Session["sc"] as ShoppingCart;
+
+            if (sc.Items == null)
+                sc.Items = new List<Item>();
+
+            sc.Items.Add(item);
+
+            HttpContext.Current.Session["sc"] = sc;
+        }
+
+        [HttpGet]
+        [Route("api/GetCart")]
+        public ShoppingCart GetCart()
+        {
+            return HttpContext.Current.Session["sc"] as ShoppingCart;//TODO HERE!
+        }
+    }
+}
